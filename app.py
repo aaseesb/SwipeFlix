e29abca7d4c15c4d55f0c2f3e6e12edfbfa8dafe
Zawar Gondal <zawar.gondal05@gmail.com>
2025-11-15 17:21:30 -0500

    added querying to scroll in flask

--- app.py
+++ app.py
@@ -1,10 +1,21 @@
 from flask import Flask, render_template
-
+import scroll
+query = []
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('home.html')
+    return render_template('home.html',)
+
+
+def update_query():
+    if query == []:
+        query = scroll.select_initial_movies()
+    else:
+        query.remove[0]
+        query.append[scroll.select_movie_weighted()]
+
+        
 
 if __name__ == '__main__':
     app.run(debug=False)
