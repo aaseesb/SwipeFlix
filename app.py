1c4a95b8b862d9a66257644fd449acb635321221
Zawar Gondal <zawar.gondal05@gmail.com>
2025-11-15 17:42:13 -0500

    added update movie funcitonality

--- app.py
+++ app.py
@@ -1,25 +1,40 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 import scroll
+
 query = []
 app = Flask(__name__)
 
 @app.route('/')
 def home():
+    updateMovie()
     return render_template('home.html',)
 
 
 def update_query():
+    global query
     if query == []:
         query = scroll.select_initial_movies()
     else:
-        query.remove[0]
-        query.append[scroll.select_movie_weighted()]
+        query.pop[0]
+        query.append(scroll.select_movie_weighted())
 
 
 @app.route('/update_movie')
 def updateMovie():
-    new_movie = render_template('movie.html')
-    return jsonify(new_movie);
+    update_query()
+    movie = query[0]
+    page = render_template('movie.html', 
+                           movieName = movie.title,
+                           releaseDate = movie.release_date,
+                           movieGenre = movie.genres,
+                           movieDesc = movie.description,
+                           movieCover = movie.cover,
+                           movieRuntime = movie.length,
+                           movieCountry = movie.country,
+                           movieLanguage = movie.language,
+                           movieActors = movie.actors
+                           )
+    return jsonify(page)
 
 
 if __name__ == '__main__':
