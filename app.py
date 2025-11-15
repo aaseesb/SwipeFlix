ec051126b19a52b2ef3c196c0788372b016dd0e0
Zawar Gondal <zawar.gondal05@gmail.com>
2025-11-15 11:13:00 -0500

    moved files around

--- app.py
+++ app.py
@@ -1,12 +1,11 @@
 from flask import Flask, render_template, request
-from bookclass import book_import
 
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('home.html')
+    return render_template('home.html', movieName="Interstellar")
 
 if __name__ == '__main__':
     app.run(debug=False)
