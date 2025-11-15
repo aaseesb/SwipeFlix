bd72a5471ca34421f484d83923b73b9a8f8975c6
Aasees <aaseesbadesha13@gmail.com>
2025-11-15 16:28:02 -0500

    completed hover animation

---
+++ static/script.js
@@ -0,0 +1,23 @@
+const overlayleft = document.getElementById('overlay-left');
+const overlayright = document.getElementById('overlay-right');
+const hiddenClass = 'hidden';
+
+
+function handleHover(event) {
+    console.log('hovered');
+
+    const trigger = event.currentTarget;
+    const targetElement = trigger.querySelector('.button-option');
+
+    if (event.type == "mouseenter") {
+        targetElement.classList.remove(hiddenClass);
+    }
+    if (event.type == "mouseleave") {
+        targetElement.classList.add(hiddenClass);
+    }
+}
+
+overlayleft.addEventListener('mouseenter', handleHover);
+overlayleft.addEventListener('mouseleave', handleHover);
+overlayright.addEventListener('mouseenter', handleHover);
+overlayright.addEventListener('mouseleave', handleHover);
